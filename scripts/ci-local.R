36291dba1996831554d8038f13d032fbb0a39a51
wilaca <willeke.acampo@miljodir.no>
2026-01-24 13:41:42 +0000

    add: local dev workflow (move to task later)

---
+++ scripts/ci-local.R
@@ -0,0 +1,41 @@
+# author: Willeke A'Campo
+# date: 2025-09-26
+# description: R script to run development workflow before committing code.
+
+# --- CLEAN START ---
+rm(list = ls())
+project_dir <- here::here()
+print(project_dir)
+setwd(project_dir)
+
+# CHECK: renv status and snapshot to dev
+renv::status(dev = TRUE)
+renv::snapshot(dev = TRUE)
+
+# CHECK: package and functions load
+devtools::load_all()
+gaupefam::hello()
+
+# CHECK: code quality
+lintr::lint_dir()               # Check only
+styler::style_dir(project_dir, exclude_dirs = c("renv", "dump", "packrat", ".git"))  # Fix
+
+# CHECK: package documentation and README
+devtools::document()      # creates docs in man/*.Rd
+devtools::build_readme()  # updates README.md based on README.Rmd
+
+# CHECK: package tests and checks
+devtools::test()
+devtools::check()
+
+# PRE-COMMIT: code quality full project
+system("pre-commit run --all-files")
+
+# DELETE: delete a function from the package
+# - remove .R file from R/
+# - remove tests from tests/testthat/
+
+# update documentation after deleting functions
+# - removes function from namespace 
+# - removes functions documentation from man/*.Rd
+devtools::document()
